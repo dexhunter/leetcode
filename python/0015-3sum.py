d69c96be9e56ca2d53ac9fd3e37f788279f2cb90
Dixing Xu <dixingxu@gmail.com>
2020-07-13 21:43:29 +0800

    update solution

--- python/0015-3sum.py
+++ python/0015-3sum.py
@@ -54,3 +54,52 @@ class Solution2:
 
         return sol
 
+class Solution3(object):
+    def threeSum(self, nums):
+	"""
+	:type nums: List[int]
+	:rtype: List[List[int]]
+	"""
+	# timsort: O(nlogn)
+	nums.sort()
+
+	# Stored val: Really fast
+	n = len(nums)
+
+	# Hash table
+	solutions = set()
+
+	# O(n): hash tables are really fast :)
+	unique_set = set(nums)
+	# covers a lot of edge cases with 2 memory lookups and 1 hash so it's worth the time
+	if len(unique_set) == 1 and 0 in unique_set and len(nums) > 2:
+	    return [[0, 0, 0]]
+
+	# O(n) but a little faster than enumerate.
+	i = 0
+	while i < n - 2:
+	    num = nums[i]
+
+	    left = i + 1
+	    right = n - 1
+
+	    # O(1/2k) where k is n-i? Not 100% sure about this one
+	    while left < right:
+		# I think its worth the memory alloc for the vars to not have to hit the list index twice. Not sure
+		# how much faster it really is. Might save two lookups per cycle.
+		left_num = nums[left]
+		right_num = nums[right]
+		s = num + left_num + right_num  # check if current sum is 0
+		if s == 0:
+		    # add to the solution set only if this triplet is unique
+		    # Hash lookup
+		    solutions.add(tuple([right_num, num, left_num]))
+		    right -= 1
+		    left += 1
+		elif s > 0:
+		    right -= 1
+		else:
+		    left += 1
+	    i += 1
+
+	return list(solutions)
