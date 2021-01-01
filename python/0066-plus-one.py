54e966546c3d73ba5c35728a987737e6d92aa647
Dixing Xu <dixingxu@gmail.com>
2021-01-01 21:44:06 +0800

    update 2 new sol

--- python/0066-plus-one.py
+++ python/0066-plus-one.py
@@ -11,4 +11,27 @@ class Solution:
             for i in range(len(digits)-len(result)):
                 result.insert(0,0)
         return result
-        
+
+class Solution2:
+    def plusOne(self, digits):
+        """recursion
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if len(digits) == 0:
+            digits = [1]
+        elif digits[-1] == 9:
+            digits = self.plusOne(digits[:-1])
+            digits.extend([0])
+        else:
+            digits[-1] += 1
+        return digits
+
+class Solution3(object):
+    def plusOne(self, digits):
+        if digits == []:  #just for case: digits = [9]
+            return [1]
+        if digits[-1] != 9:
+            return digits[:-1]+[digits[-1]+1]
+        else:
+            return self.plusOne(digits[:-1])+[0]
